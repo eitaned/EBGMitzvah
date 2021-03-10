c1252bdf089788b3a75c349bc7c1b7352f90ec46
eitaned <80429645+eitaned@users.noreply.github.com>
2021-03-10 17:37:56 -0500

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"604947ffc3a5f1001e7b9de5",
+				"604947ffc3a5f1001e7b9de7"
+			]
+		},
+		"604947ffc3a5f1001e7b9de5": {
+			"id": "604947ffc3a5f1001e7b9de5",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"604947ffc3a5f1001e7b9de7": {
+			"id": "604947ffc3a5f1001e7b9de7",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
